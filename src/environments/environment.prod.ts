436fd4a520bc39c480f0bb002d1d679f310288dc
Fernando Vega <juanfe.vega@gmail.com>
2021-11-09 00:48:09 -0500

    Add environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
+const API_KEY = '2913a984521a45168cf500e64584b896';
 export const environment = {
-  production: true
+  production: true,
+  api_url: `https://api.rawg.io/api/platforms?key=${API_KEY}`
 };
